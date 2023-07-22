15a5afaadf786a6ce8bc618c1be8a8883bea965f
Yufeng Luo <brookluo@yahoo.com>
2023-07-22 16:34:09 -0500

    Remove all write functions in __init__.py

--- py/desispec/io/__init__.py
+++ py/desispec/io/__init__.py
@@ -14,17 +14,17 @@ warnings.filterwarnings('ignore', message="'.*nanomaggies.* did not parse as fit
 warnings.filterwarnings('ignore', message=".*'10\*\*6 arcsec.* did not parse as fits unit.*")
 
 # from .download import download, filepath2url
-from .fiberflat import read_fiberflat, write_fiberflat
-from .fibermap import read_fibermap, write_fibermap, empty_fibermap
+from .fiberflat import read_fiberflat
+from .fibermap import read_fibermap, empty_fibermap
 from .filters import load_filter,load_legacy_survey_filter
-from .fluxcalibration import (read_stdstar_templates, write_stdstar_models,
+from .fluxcalibration import (read_stdstar_templates,
                               read_stdstar_models, read_flux_calibration,
-                              write_flux_calibration, read_average_flux_calibration)
-from .spectra import (read_spectra, write_spectra, read_frame_as_spectra,
+                              read_average_flux_calibration)
+from .spectra import (read_spectra, read_frame_as_spectra,
                       read_tile_spectra)
-from .frame import read_meta_frame, read_frame, write_frame
-from .xytraceset import read_xytraceset, write_xytraceset
-from .image import read_image, write_image
+from .frame import read_meta_frame, read_frame
+from .xytraceset import read_xytraceset
+from .image import read_image
 from .meta import (findfile, get_exposures, get_files, get_raw_files,
                    rawdata_root, specprod_root, validate_night, qaprod_root,
                    get_pipe_rundir, get_pipe_scriptdir, get_pipe_database,
@@ -32,18 +32,18 @@ from .meta import (findfile, get_exposures, get_files, get_raw_files,
                    get_nights, get_pipe_nightdir, find_exposure_night,
                    shorten_filename, get_readonly_filepath)
 from .params import read_params
-from .qa import (read_qa_frame, read_qa_data, write_qa_frame, write_qa_brick,
-                 load_qa_frame, write_qa_exposure, write_qa_multiexp, load_qa_multiexp,
+from .qa import (read_qa_frame, read_qa_data,
+                 load_qa_frame, load_qa_multiexp,
                  qafile_from_framefile)
-from .exposure_tile_qa import (read_exposure_qa, write_exposure_qa, read_tile_qa, write_tile_qa)
-from .raw import read_raw, write_raw
-from .sky import read_sky, write_sky
-from .skycorr import (read_skycorr, write_skycorr, read_skycorr_pca, write_skycorr_pca)
-from .skygradpca import read_skygradpca, write_skygradpca
+from .exposure_tile_qa import (read_exposure_qa, read_tile_qa)
+from .raw import read_raw
+from .sky import read_sky
+from .skycorr import (read_skycorr, read_skycorr_pca)
+from .skygradpca import read_skygradpca
 from .tpcorrparam import read_tpcorrparam
 from .table import read_table
 from .util import (header2wave, fitsheader, native_endian, makepath,
-                   write_bintable, iterfiles,
+                   iterfiles,
                    healpix_subdirectory, replace_prefix)
 
 # Why is this even here?
